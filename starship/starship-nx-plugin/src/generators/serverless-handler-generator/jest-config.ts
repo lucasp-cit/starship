48d7e2edadf98d2bdb0f4650f5348f564f4e662f
Lucas Bragalia Polo <lbraga02@kenvue.com>
2023-12-27 09:43:39 -0800

    adjusting imports from the jest-config generated by the generator

--- starship/starship-nx-plugin/src/generators/serverless-handler-generator/jest-config.ts
+++ starship/starship-nx-plugin/src/generators/serverless-handler-generator/jest-config.ts
@@ -1,9 +1,9 @@
 import { Tree } from '@nx/devkit';
-import { jestProjectGenerator } from '@nx/jest';
-import { JestProjectSchema } from '@nx/jest/src/generators/jest-project/schema';
+import { configurationGenerator } from '@nx/jest';
+import { JestProjectSchema } from '@nx/jest/src/generators/configuration/schema';
 
 export const addJest = async (host: Tree, projectName: string) => {
-  await jestProjectGenerator(host, <JestProjectSchema>{
+  await configurationGenerator(host, <JestProjectSchema>{
     project: projectName,
     setupFile: 'none',
     testEnvironment: 'node',
