6ca707a05e8d28cd10a2e973ca4651a13fd8a8f1
Lucas Bragalia Polo <lbraga02@kenvue.com>
2024-01-22 18:23:26 -0800

    testing

--- jenkins-build.groovy
+++ jenkins-build.groovy
@@ -30,8 +30,17 @@ def deploy(env) {
 
 def mergeAndCreateTagOnMain(releaseNumberFromBranch) {
     echo ".......................Merging and Creating tag ${releaseNumberFromBranch} on Main......................."
-    sh "git fetch"
-    sh "git branch -a"
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name: 'main']],
+        extensions: [
+            [$class: 'CloneOption', noTags: true, reference: '', shallow: true]
+        ],
+        submoduleCfg: [],
+        userRemoteConfigs: [
+            [ credentialsId: 'lucasp-ciandt', url: 'https://github.com/lucasp-cit/starship.git']
+        ]
+    ])
     sh "git checkout main"
     sh "git merge --squash release/${releaseNumberFromBranch}"
     sh "git tag -a tag/${releaseNumberFromBranch} -m \"Release Tag for version: ${${releaseNumberFromBranch}}\""
