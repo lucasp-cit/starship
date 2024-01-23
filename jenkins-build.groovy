ccd25128acf893e8b7c59477eb58f844ed1041ad
Lucas Bragalia Polo <lbraga02@kenvue.com>
2024-01-22 20:15:53 -0800

    aaaa

--- jenkins-build.groovy
+++ jenkins-build.groovy
@@ -50,6 +50,9 @@ def mergeAndCreateTagOnMain(releaseNumberFromBranch) {
     
     sh "git fetch"
     sh "git checkout origin/main"
+    sh "git status"
+    sh "git branch"
+    sh "git pull"
     sh "git merge origin/release/${releaseNumberFromBranch}"
     // sh "git tag -a tag/${releaseNumberFromBranch} -m \"Release Tag for version: ${${releaseNumberFromBranch}}\""
     // sh "git push origin tag/${releaseNumberFromBranch}"
